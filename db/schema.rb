2a7b69865f24f2ea36c6dfa58d6b92f345fa24cc
Aman Tank <amantank187@gmail.com>
2021-04-05 14:57:25 +0100

    Fixed webpack/bootstrap error and fixed the article migration as it was not inputting the time for when the article was created

--- db/schema.rb
+++ db/schema.rb
@@ -18,8 +18,8 @@ ActiveRecord::Schema.define(version: 2021_04_01_120327) do
   create_table "articles", force: :cascade do |t|
     t.string "title"
     t.text "description"
-    t.datetime "create_at"
-    t.datetime "updated_at"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
   end
 
   create_table "users", force: :cascade do |t|
