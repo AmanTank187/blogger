50b96481a80b5d7c622d412eddf1b88cdf655dec
Aman Tank <amantank187@gmail.com>
2021-04-13 16:06:40 +0100

    Create a user action has been implemented into the controller

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,26 @@
+class UsersController < ApplicationController
+
+    def new 
+        #Initialising user
+        @user = User.new 
+    end 
+
+    def create 
+        @user = User.new(user_params)
+
+        if @user.save 
+            flash[:notice] = "Welcome to Blogger  #{@user.username} ! You have successfully signed up"
+            redirect_to articles_path 
+        else 
+            render 'new'
+        end 
+    end
+
+    private 
+
+    def user_params
+        params.require(:user).permit(:username, :email, :password)
+    end 
+
+
+end 
