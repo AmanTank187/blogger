6391b28c9c59e0c150c992fe142eac55c3871133
Aman Tank <amantank187@gmail.com>
2021-04-27 15:40:14 +0100

    Tidied up views and connected some buttons

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -22,7 +22,7 @@ class UsersController < ApplicationController
     def update 
         if @user.update(user_params)
             flash[:notice] = "Profile was updated sccessfully."
-            redirect_to articles_path
+            redirect_to @user 
         else 
             render 'edit'
         end 
