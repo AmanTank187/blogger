1a511874c072cd93c369b985a1569e1f1549127e
Aman <Amantank187@gmail.com>
2021-08-31 16:46:12 +0100

    Feature test for username less than 3 characters

--- spec/features/user_authentication/sign_up_spec.rb
+++ spec/features/user_authentication/sign_up_spec.rb
@@ -9,4 +9,13 @@ RSpec.feature "Sign-Up", type: :feature do
       click_button("Sign up")
       expect(page).to have_content("Amant")
     end
+
+    scenario "Can't sign up if username is less than 3 characters" do
+      visit "/signup"
+      fill_in "user_username", with: "Am"
+      fill_in "user_email", with: "Amant@example.com"
+      fill_in "user_password", with: "Amant"
+      click_button("Sign up")
+      expect(page).to have_content("Username is too short (minimum is 3 characters)")
+    end
 end 
